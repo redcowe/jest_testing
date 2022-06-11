e8a280ae5285a627269ad5288a39c9cc03a6ea14
Joshua Cowell <joshuacowell@hotmail.com>
2022-06-11 17:16:53 +0000

    added tests for calculator

--- index.test.js
+++ index.test.js
@@ -1,6 +1,8 @@
 const index = require("./index")
-const capitalize = index.capitalize
-const reverse = index.reverse
+const capitalize = index.capitalize;
+const reverse = index.reverse;
+const calculator = index.calculator;
+const caesarCipher = index.caesarCipher;
 
 describe("String tests", () => {
     it('first letter capital', () => { 
@@ -10,4 +12,20 @@ describe("String tests", () => {
      it('reverse string', () => {
         expect(reverse("car")).toBe("rac");
      });
+
+     it('calculator add', () => {
+      expect(calculator.add(1, 1)).toBe(2);
+     })
+
+     it('calculator subtract', () => {
+      expect(calculator.subtract(2,1)).toBe(1);
+     })
+     
+     it('calculator multiply', () => {
+      expect(calculator.multiply(2,2)).toBe(4);
+     })
+
+     it('calculator divide', () => {
+      expect(calculator.divide(4,2)).toBe(2);
+     })
 })
